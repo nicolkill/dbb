9f60205d49c452af6757909b29c4215195ebcce4
Nicol Acosta <nicolacosta8@gmail.com>
2023-04-19 10:45:08 -0600

    added missing lines

--- lib/dbb/content/table.ex
+++ lib/dbb/content/table.ex
@@ -4,11 +4,18 @@ defmodule Dbb.Content.Table do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+
+  @required_fields [:schema, :reference, :data]
+  @fields @required_fields
+  @soft_delete_fields [:deleted_at]
+
   schema "table" do
     field :data, :map
-    field :deleted_at, :naive_datetime
     field :reference, Ecto.UUID
     field :schema, :string
+
+    # soft delete field
+    field :deleted_at, :naive_datetime
 
     timestamps()
   end
@@ -16,7 +23,12 @@ defmodule Dbb.Content.Table do
   @doc false
   def changeset(table, attrs) do
     table
-    |> cast(attrs, [:schema, :reference, :data, :deleted_at])
-    |> validate_required([:schema, :reference, :data, :deleted_at])
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
+  end
+  def changeset_delete(table, attrs) do
+    table
+    |> cast(attrs, @soft_delete_fields)
+    |> validate_required(@soft_delete_fields)
   end
 end
