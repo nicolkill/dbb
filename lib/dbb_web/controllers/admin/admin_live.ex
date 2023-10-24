2bd7453443987655846e4221a84916ca8ed08960
Nicol Acosta <nicolacosta8@gmail.com>
2023-10-24 00:48:21 -0600

    feat: added basic component list

--- lib/dbb_web/controllers/admin/admin_live.ex
+++ lib/dbb_web/controllers/admin/admin_live.ex
@@ -6,8 +6,12 @@ defmodule DbbWeb.Admin.AdminLive do
 #  end
 
   def mount(_params, _session, socket) do
-    temperature = 70
-    {:ok, assign(socket, :temperature, temperature)}
+    schemas =
+      Dbb.Schema.get_config()
+      |> Map.get("schemas")
+      |> Enum.map(&Map.get(&1, "name"))
+
+    {:ok, assign(socket, :schemas, schemas)}
   end
   
 end
